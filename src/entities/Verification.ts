c0da97a8349f2610eaea8950904285ae92074ae0
KishoreNewton <kishorenke@gmail.com>
2020-10-12 01:26:54 +0530

    createkey

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,6 +1,7 @@
 import { verificationTarget } from 'src/types/types';
 import {
   BaseEntity,
+  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
@@ -8,11 +9,14 @@ import {
   UpdateDateColumn
 } from 'typeorm';
 
+const PHONE: verificationTarget = 'PHONE';
+const EMAIL: verificationTarget = 'EMAIL';
+
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: 'text', enum: ["PHONE", "EMAIL"] })
+  @Column({ type: 'text', enum: [PHONE, EMAIL] })
   target: verificationTarget;
 
   @Column({ type: 'text' })
@@ -26,4 +30,15 @@ class Verification extends BaseEntity {
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
+
+  @BeforeInsert()
+  private createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 1000000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random().toString(36).substr(2);
+    } else {
+      throw new Error('Something went wrong');
+    }
+  }
 }
