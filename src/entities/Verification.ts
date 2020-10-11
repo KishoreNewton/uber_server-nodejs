d1cebeec588293d7e22741a7681b7bfd4a09fc08
KishoreNewton <kishorenke@gmail.com>
2020-10-11 17:23:46 +0530

    added: Verification.ts in entities

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,1 +1,28 @@
-import { BaseEntity, Column, Entity } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm';
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: 'text' })
+  target: string;
+
+  @Column({ type: 'text' })
+  payload: string;
+
+  @Column({ type: 'text' })
+  key: string;
+
+  @Column({ type: 'boolean', default: false })
+  used: boolean;
+
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+}
