e95ac94f72eba2098796c5c23b6d3e492eea16aa
KishoreNewton <kishorenke@gmail.com>
2020-10-12 01:16:46 +0530

    graphql interface added

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,16 +1,39 @@
-export const typeDefs = ["type Query {\n  sayBye: String!\n  sayHello(name: String!): SayHelloResponse!\n}\n\ntype SayHelloResponse {\n  type: String!\n  error: Boolean!\n}\n"];
+export const typeDefs = ["type Query {\n  user: User\n}\n\ntype User {\n  id: Int!\n  email: String\n  firstName: String!\n  lastName: String!\n  facebookId: String\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedEmail: Boolean!\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float!\n  latLat: Float!\n  lastOrientation: Float\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayBye: string;
-  sayHello: SayHelloResponse;
+  user: User | null;
 }
 
-export interface SayHelloQueryArgs {
-  name: string;
+export interface User {
+  id: number;
+  email: string | null;
+  firstName: string;
+  lastName: string;
+  facebookId: string | null;
+  age: number | null;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedEmail: boolean;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  createdAt: string;
+  updatedAt: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLng: number;
+  latLat: number;
+  lastOrientation: number | null;
 }
 
-export interface SayHelloResponse {
-  type: string;
-  error: boolean;
+export interface Verification {
+  id: number;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
